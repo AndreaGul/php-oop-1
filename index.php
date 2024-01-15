614e2264c5a342ae9f7b3a4afec1f8094821b647
AndreaGul <andreagulino99@gmail.com>
2024-01-15 23:16:31 +0100

    feat:definisco il metodo construct nella classe movie

--- index.php
+++ index.php
@@ -4,14 +4,21 @@
 class movie{
   public $title;
   public $duration;
+  private $genere;
+
+  function __construct($genere)
+  {
+    $this->genere = $genere;
+  }
+  
 }
 
 //creao un instanza della classe movie -> il signore degli anelli
-$signAnelli1= new movie();
-$signAnelli1->title = 'il signore degli anelli';
-$signAnelli1->duration = '2h 58m'
+$signAnelli1= new movie('Fantasy');
+$signAnelli1->title = 'Il signore degli anelli';
+$signAnelli1->duration = '2h 58m';
 
-
+var_dump($signAnelli1);
 ?>
 
 
@@ -20,8 +27,7 @@ $signAnelli1->duration = '2h 58m'
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>OOP 1 movies
-  </title>
+  <title>OOP 1 movies</title>
 </head>
 <body>
   
